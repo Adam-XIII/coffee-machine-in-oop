94262c6d406ba01b3f23138aa5b726a9daf14a8b
Adam_XIII <adam.krzysztala@gmail.com>
2024-04-14 16:41:21 +0200

    Small typo fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ is_on=True
 
 while is_on:
     options=menu.get_items()
-    choice=input(f"What would you like? {options} ")
+    choice=input(f"What would you like? {options} : ")
     if choice=="off":
         is_on=False
     elif choice=="report":
